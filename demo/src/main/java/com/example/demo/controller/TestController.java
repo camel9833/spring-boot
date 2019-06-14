183e04b88618a618cb7c78c93f0e227e19fc6ca0
camel9833 <camel9833@daum.net>
2019-06-15 00:57:53 +0900

    11

--- demo/src/main/java/com/example/demo/controller/TestController.java
+++ demo/src/main/java/com/example/demo/controller/TestController.java
@@ -21,70 +21,34 @@ import com.squareup.okhttp.Response;
 public class TestController {
 	@RequestMapping("/test")
 	public String getTest() {
-//		return "{\"result\":true,\"resultMsg\":\"성공입니다.\"}";
+		String json = "";
 		String url = "https://jsonplaceholder.typicode.com/posts";
-//		String jsonString = get(uri);
-//		System.out.println(jsonData);
 
-	//---------------------------------------------------------
-		  try {
-			    OkHttpClient client = new OkHttpClient();
-
-//			    String url = SERVER_CONFIGURATION.ADDRESS + ":" +
-//			        SERVER_CONFIGURATION.PORT + "/v1/userinfo";
-
-			    Request request = new Request.Builder()
-			        .addHeader("Authorization", "TEST AUTH")
-			        .url(url)
-			        .build();
-			    Response response = client.newCall(request)
-			        .execute();
-
-			    String result = response.body().string();
-			    
-//			    System.out.println(result);
-
-			    Gson gson = new Gson();
-			    List<TestVO> info = Arrays.asList(gson.fromJson(result, TestVO[].class));
-
-			    System.out.println(info.get(0).getId());
-//			    Log.i("id: " + info.id);
-//			    Log.i("name: " + info.name);
-
-//			    return true;
-			  } catch(Exception e) {
-			    e.printStackTrace();
-			  }
-		
-		
-		
-	//---------------------------------------------------------
-		
-	return "111";
-
-	}
-
-/*	
-	public String get(String requestURL) {
-
-		String message = "-1";
+		// ---------------------------------------------------------
 		try {
 			OkHttpClient client = new OkHttpClient();
 			Request request = new Request.Builder()
-//					.addHeader("x-api-key", RestTestCommon.API_KEY)
-					.url(requestURL).build(); // GET Request
-
-			// 동기 처리시 execute함수 사용
+					         .addHeader("Authorization", "TEST AUTH")
+					         .url(url)
+					         .build();
 			Response response = client.newCall(request).execute();
+			String result = response.body().string();
+			
+			// string --> list
+			Gson gson = new Gson();
+			List<TestVO> info = Arrays.asList(gson.fromJson(result, TestVO[].class));
 
-			// 출력
-			message = response.body().string();
-//			System.out.println(message);
+			// list -> string
+			json = gson.toJson(info);
+
 		} catch (Exception e) {
-			System.err.println(e.toString());
+			e.printStackTrace();
 		}
-		return message;
+
+		// ---------------------------------------------------------
+
+		return json;
+
 	}
-*/
-	
+
 }
