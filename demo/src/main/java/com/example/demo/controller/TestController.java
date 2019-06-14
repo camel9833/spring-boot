b83ccd51fc747506e6a4be60d9a56946dbd432ee
camel9833 <camel9833@daum.net>
2019-06-14 23:11:16 +0900

    111

--- demo/src/main/java/com/example/demo/controller/TestController.java
+++ demo/src/main/java/com/example/demo/controller/TestController.java
@@ -1,5 +1,8 @@
 package com.example.demo.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -8,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.VO.TestVO;
+import com.google.gson.Gson;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
@@ -17,19 +22,49 @@ public class TestController {
 	@RequestMapping("/test")
 	public String getTest() {
 //		return "{\"result\":true,\"resultMsg\":\"성공입니다.\"}";
-		String uri = "https://jsonplaceholder.typicode.com/posts";
-		String jsonString = get(uri);
+		String url = "https://jsonplaceholder.typicode.com/posts";
+//		String jsonString = get(uri);
 //		System.out.println(jsonData);
 
 	//---------------------------------------------------------
+		  try {
+			    OkHttpClient client = new OkHttpClient();
 
+//			    String url = SERVER_CONFIGURATION.ADDRESS + ":" +
+//			        SERVER_CONFIGURATION.PORT + "/v1/userinfo";
+
+			    Request request = new Request.Builder()
+			        .addHeader("Authorization", "TEST AUTH")
+			        .url(url)
+			        .build();
+			    Response response = client.newCall(request)
+			        .execute();
+
+			    String result = response.body().string();
+			    
+//			    System.out.println(result);
+
+			    Gson gson = new Gson();
+			    List<TestVO> info = Arrays.asList(gson.fromJson(result, TestVO[].class));
+
+			    System.out.println(info.get(0).getId());
+//			    Log.i("id: " + info.id);
+//			    Log.i("name: " + info.name);
+
+//			    return true;
+			  } catch(Exception e) {
+			    e.printStackTrace();
+			  }
+		
+		
 		
 	//---------------------------------------------------------
 		
-	return jsonString;
+	return "111";
 
 	}
 
+/*	
 	public String get(String requestURL) {
 
 		String message = "-1";
@@ -50,5 +85,6 @@ public class TestController {
 		}
 		return message;
 	}
-
+*/
+	
 }
